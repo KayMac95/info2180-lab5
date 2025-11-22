d46ae4c18a22bbf2b84ce5a16d28cb157899a5cc
Kayon McLaren <Kayon0987@gmail.com>
2025-11-22 16:27:24 -0500

    Updated files

---
+++ world.js
@@ -0,0 +1,29 @@
+$( document ).ready(function(){
+    let searchButton = $("#lookup");
+    searchButton.click(function(){
+
+        let country =  $("#country").val().trim(); // removal of any leading or trailing whitspace
+        //userInput = userInput.toLowerCase(); // convert input to lowercase (will be used for handling retrieval consistency)
+
+        $.ajax({
+            url: "world.php",
+            type: "GET",
+            dataType: "json",
+            data: {
+                country: country
+            }
+        })
+        .done(function(result){
+            
+            //let result_div = $("#result");
+            //result_div.html(result);
+            console.log(result);
+
+            
+
+        })
+        .fail(function(result){
+            console.log("Something went wrong.");
+        });
+    });
+});
--- world.php
+++ world.php
@@ -1,17 +1,44 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
+$country = $_GET['country'] ?? '';
 
+
+
+function country_lookup($country){
+  global $host;
+  global $username;
+  global $dbname;
+  global $password;
+  global $country;
+
+  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  $countrylist = [];
+  while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $countrylist[$row['name']] = $row;
+  };
+  return $countrylist;
+}
+
+echo json_encode(country_lookup($country));
+?>
+
+<?php
+/*
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+*
 ?>
+
+/*
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
 </ul>
+*/
