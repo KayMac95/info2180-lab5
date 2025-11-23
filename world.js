39cc989c59d71d4f7726c5e4cf66655ecab5e782
Kayon McLaren <Kayon0987@gmail.com>
2025-11-23 00:25:42 -0500

    Updated files

--- world.js
+++ world.js
@@ -1,25 +1,52 @@
+
 $( document ).ready(function(){
     let searchButton = $("#lookup");
     searchButton.click(function(){
 
         let country =  $("#country").val().trim(); // removal of any leading or trailing whitspace
-        //userInput = userInput.toLowerCase(); // convert input to lowercase (will be used for handling retrieval consistency)
+        
+        let flag = 'country';
 
         $.ajax({
             url: "world.php",
             type: "GET",
             dataType: "json",
             data: {
-                country: country
+                country: country,
+                flag: flag
             }
         })
         .done(function(result){
             
             let result_div = $("#result");
             result_div.html(result);
-            //console.log(result);
 
+        })
+        .fail(function(result){
+            console.log("Something went wrong.");
+        });
+    });
+
+    let cityButton = $("#city_lookup");
+    cityButton.click(function(){
+        
+        let country =  $("#country").val().trim(); // removal of any leading or trailing whitspace
+        let flag = 'city';
+
+        $.ajax({
+            url: "world.php",
+            type: "GET",
+            dataType: "json",
+            data: {
+                country: country,
+                flag: flag
+            }
+        })
+        .done(function(result){
             
+            let result_div = $("#result");
+            result_div.html(result);
+
 
         })
         .fail(function(result){
--- world.php
+++ world.php
@@ -4,6 +4,7 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 $country = $_GET['country'] ?? '';
+$flag = $_GET['flag'] ?? '';
 
 
 
@@ -13,9 +14,16 @@ function country_lookup($country){
   global $dbname;
   global $password;
   global $country;
+  
 
   $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+
+  $country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
+  $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+  $like_country = "%{$country}%";
+  $stmt->bindParam(':country', $like_country, PDO::PARAM_STR);
+  $stmt->execute();
+
   $countrylist = [];
   while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
     $countrylist[$row['name']] = $row;
@@ -23,30 +31,77 @@ function country_lookup($country){
   return $countrylist;
 }
 
+function city_lookup($country){
+  global $host;
+  global $username;
+  global $dbname;
+  global $password;
+  global $country;
+
+
+  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+
+  $country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
+  $stmt = $conn->prepare("SELECT countries.name AS country_name, cities.name AS city_name FROM countries JOIN cities ON countries.code = cities.country_code WHERE countries.name LIKE :country");
+  $like_country = "%{$country}%";
+  $stmt->bindParam(':country', $like_country, PDO::PARAM_STR);
+  $stmt->execute();
+
+  $countrylist = [];
+  while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $countrylist[$row['country_name']] = $row;
+  };
+  return $countrylist;
+}
+
 
 function build_Table($countries){
-  $tab = "<Table>";
+  $tab = "<Table class='res_table'>";
   $tr_strt = "<tr>";
   $tr_end = "</tr>";
   $th_strt = "<th>";
   $th_end = "</th>";
   $td_strt = "<td>";
   $td_end = "</td>";
+  global $flag;
 
-  $tab .= $tr_strt . $th_strt . "Country Name" . $th_end . $th_strt . "Continent" . $th_end .
-  $th_strt . "Independence" . $th_end . $th_strt . "Head of State" . $th_end  . $tr_end; 
+  if ($flag == "country"){
 
-  foreach ($countries as $country){
-    $tab .= $tr_strt . $td_strt . $country["name"] . $td_end . $td_strt . $country["continent"] . $td_end .
-    $td_strt . $country["independence_year"] . $td_end . $td_strt . $country["head_of_state"] . $td_end  . $tr_end;
+    // Set up the header row
+    $tab .= $tr_strt . $th_strt . "Country Name" . $th_end . $th_strt . "Continent" . $th_end .
+    $th_strt . "Independence" . $th_end . $th_strt . "Head of State" . $th_end  . $tr_end; 
+
+    // Set up the rows for each country
+    foreach ($countries as $country){
+      $tab .= $tr_strt . $td_strt . htmlspecialchars($country["name"]) . $td_end . $td_strt . htmlspecialchars($country["continent"]) . $td_end .
+      $td_strt . htmlspecialchars($country["independence_year"]) . $td_end . $td_strt . htmlspecialchars($country["head_of_state"]) . $td_end  . $tr_end;
+    }
+    $tab .= "</Table>";
+    return $tab;
   }
-  $tab .= "</Table>";
 
-  return $tab;
+  else{
+    // Set up the header row
+    $tab .= $tr_strt . $th_strt . "Country" . $th_end . $th_strt . "City" . $th_end . $tr_end;
+
+
+    // Set up the rows for each country + city
+    foreach ($countries as $country){
+      $tab .= $tr_strt . $td_strt . htmlspecialchars($country["country_name"]) . $td_end . $td_strt . htmlspecialchars($country["city_name"]) . $td_end . $tr_end;
+    }
+    $tab .= "</Table>";
+    return $tab;
+  }
+  
   
 }
+if ($flag == 'country'){
+  echo json_encode(build_Table(country_lookup($country)));
+}
+else{
+  echo json_encode(build_Table(city_lookup($country)));
+}
 
-echo json_encode(build_Table(country_lookup($country)));
 ?>
 
 <?php
